57b44d0816a72a5a5a7c36da83042120838c825a
EnzoSoares <enzosoares@protonmail.com>
2024-04-06 13:32:19 -0300

    implementing Insert, append, prepend and toSlice in LinkedList

--- data-structures/linked-list/linked_list.go
+++ data-structures/linked-list/linked_list.go
@@ -23,10 +23,10 @@ func NewLinkedList(list []any) *LinkedList[any] {
 	linkedList := &LinkedList[any]{
 		tail:   firstNode,
 		head:   firstNode,
-		length: 0,
+		length: 1,
 	}
 
-	for i := len(list) - 1; i >= 0; i-- {
+	for i := len(list) - 2; i >= 0; i-- {
 		linkedList.length++
 		node := &Node[any]{
 			value: list[i],
@@ -46,11 +46,71 @@ func (l *LinkedList[T]) Append(item T) {
 
 	l.tail.next = node
 	l.tail = node
+	l.length++
+}
+
+func (l *LinkedList[T]) Prepend(item T) {
+	node := &Node[T]{
+		value: item,
+		next:  l.head,
+	}
+
+	l.head = node
+	l.length++
+}
+
+func (l *LinkedList[T]) Insert(index int, item T) {
+	var previousNode *Node[T]
+
+	newNode := &Node[T]{
+		value: item,
+		next:  nil,
+	}
+
+	for i := 0; i <= l.length-1 && i <= index-1; i++ {
+		if i == 0 {
+			previousNode = l.head
+		} else {
+			previousNode = previousNode.next
+		}
+	}
+
+	l.length++
+
+	if previousNode == nil {
+		newNode.next = l.head
+		l.head = newNode
+		return
+	}
+
+	if previousNode == l.tail {
+		l.tail.next = newNode
+		newNode.next = nil
+		l.tail = newNode
+		return
+	}
+
+	newNode.next = previousNode.next
+	previousNode.next = newNode
+}
+
+func (l *LinkedList[T]) ToSlice() []T {
+	currentNode := l.head
+	slice := []T{}
+	for i := 0; currentNode != nil; i++ {
+		slice = append(slice, currentNode.value)
+		currentNode = currentNode.next
+	}
+
+	return slice
 }
 
 func main() {
-	list := []any{"A", "B", "C"}
+	list := []any{"B", "C", "D"}
 	linkedList := NewLinkedList(list)
-	linkedList.Append("D")
-	fmt.Print(linkedList.tail.value)
+	linkedList.Prepend("A")
+	linkedList.Append("F")
+	linkedList.Insert(7, "E")
+	// fmt.Print(linkedList.head.next.next.value)
+	fmt.Print(linkedList.ToSlice(), " ", linkedList.length, "\n")
 }
